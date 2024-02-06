c11fb953731df167d5f3d93acd860eb56188215e
Peter <contact@peter.dev.br>
2024-02-06 17:56:12 -0300

    Add better refer parser

--- whois.py
+++ whois.py
@@ -7,14 +7,15 @@ IANA_WHOIS_SERVER = "whois.iana.org"
 
 def _get_refer_server(res):
     try:
-        return re.findall(r'refer:\s*(.+)\n', res)[0]
+        urls = filter(lambda x: x.startswith("refer") or x.startswith("whois"), res.split("\n"))
+        return list(map(lambda x: x.split(":")[1].strip(), urls))[0]
     except IndexError:
         return None
 
 def query(target, server = IANA_WHOIS_SERVER, port = WHOIS_PORT):
     whois_ip_addr = utils.find_ip(server)
 
-    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    conn = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
     conn.connect((whois_ip_addr, port))
 
     conn.send(f"{target}\r\n".encode())
